7daeef004ac64c1fc89ae52a79a96e028df5124c
Charps-Studios <>
2021-03-21 12:55:58 +1300

    Final commit, forgot to commit before, wow.

--- src/main.cpp
+++ src/main.cpp
@@ -1,185 +1,211 @@
 #include "vex.h"
-#include "sstream"
-#include "console.h"
-#include "auton.h"
-#include "ui.h"
-#include "vector2f.h"
-
+#include <sstream>
+#include <cstdlib>
 /**
 * This file is the starting off point of the program.
 *
 * @author Bailey Moir
 */
 
-namespace Syntech {
-  /*
-  * 1 = left
-  * -1 = right
-  */
-  int side = 1;
-  bool mid = false;  
+/*- Global Variables -*/
+  bool complete = false;
+  float speed = 1;
 
-  /*
-  * true = arcade.
-  * false = tank.
-  */
-  bool move = false;
-  bool stick = false;
-  /*
-  * 0 = Not run, use auton configuration buttons.
-  * 1 = Auton is complete, just needs to clear screen.
-  * 2 = Cleared the screen, no more to do.
-  */
-  int autonCompletitonSteps = 0;
+  const float wheelHeight = 10.4;
+  const float wheelCircumference = wheelHeight * 3.14159265359;
+/*--------------------*/
 
-  void autonomous(void) {
-    GameObjects::inertialSensor.calibrate();
-    waitUntil(!GameObjects::inertialSensor.isCalibrating());
-    if (mid) {
-      Auton::move(10, 1000);
-      Auton::turnDeg(45 * side, 2000);
-      Auton::move(5, 1000);
-      Auton::intake(2000, Auton::globalSpeed);
-      Auton::move(-5, 1000);
-      Auton::turnDeg(135 * side, 4000);
-      Auton::move(60.96, 8000);
-      Auton::turnDeg(-45 * side, 2000);
-      Auton::move(5, 1000);
-      Auton::intake(2000, Auton::globalSpeed);
-      Auton::move(-30, 2000);
-    } else {
-      GameObjects::intakeLeft.spin(directionType::rev, Auton::globalSpeed, rpm);
-      GameObjects::intakeRight.spin(directionType::rev, Auton::globalSpeed, rpm);
-      Auton::move(30.345, 3000);
-      Auton::turnDeg(-45 * side, 5000);
-      Auton::move(10, 2000);
-      GameObjects::lift.spin(directionType::rev, Auton::globalSpeed, rpm);
-      wait(2000, msec);
-      Auton::move(-30, 2000);
-      GameObjects::intakeLeft.stop(coast);
-      GameObjects::intakeRight.stop(coast);
-      GameObjects::lift.stop(coast);
+/*---  Motor Setup ---*/
+  competition Competition = competition();
+  controller Controller = controller();
+
+  motor leftMotor = motor(PORT4, false);
+  motor rightMotor = motor(PORT9, true);
+  motor leftFMotor = motor(PORT3, false);
+  motor rightFMotor = motor(PORT1, true);
+  motor intakeLeft = motor(PORT8, false);
+  motor intakeRight = motor(PORT20, true);
+  motor firstLift = motor(PORT2, true);
+  motor secondLift = motor(PORT10, false); //Higher
+
+  inertial inertialSensor = inertial(PORT11);
+/*--------------------*/
+
+/*- Auton Func & Cls -*/
+  int consoleLine = 0;
+  class Console { //480 x 240
+  public:
+    //Constructor
+    static void init() { 
+      Brain.Screen.clearScreen();
+      Brain.Screen.setFillColor("#ff006f");
+      Brain.Screen.drawRectangle(0, 0, 480, 240);
+      Brain.Screen.setFillColor("#ffffff");
+      Brain.Screen.drawRectangle(10, 10, 460, 220);
+      //Brain.Screen.drawImageFromFile("Syntech.png", 0, 0);
     }
-    autonCompletitonSteps++;
-  }
-  
-  // Resets the controller screen, refresing the values.
-  void resetControllerScreen() {
-    GameObjects::Controller.Screen.clearScreen();
-    
-    // Cursor is where it prints on the screen.
-    GameObjects::Controller.Screen.setCursor(1,1);
-    GameObjects::Controller.Screen.print("Syntech:");
 
-    // Print the state of the current movement on the controller.
-    GameObjects::Controller.Screen.setCursor(2,1);
-    if (move) GameObjects::Controller.Screen.print("Movement: arcade");
-    else GameObjects::Controller.Screen.print("Movement: tank");
-    
-    // Prints teh state of if the movement is sticky on the controller.
-    GameObjects::Controller.Screen.setCursor(3,1);
-    if (stick) GameObjects::Controller.Screen.print("Sticky: true");
-    else GameObjects::Controller.Screen.print("Sticky: false");
-  }
+    /*static void print(const char* text) {
+      if (consoleLine == 10) { //Over
+        consoleLine = 0;
+        init();
+      }
+      Brain.Screen.setFillColor("#ffffff");
+      Brain.Screen.setPenColor("000000");
+      Brain.Screen.setCursor(consoleLine + 2, 2);
+      Brain.Screen.print(text);
+      consoleLine++;
+    }*/
+    static void print(const char* text) {
+      if (consoleLine == 10) { //Over
+        consoleLine = 0;
+        init();
+      }
+      Brain.Screen.setFillColor("#ffffff");
+      Brain.Screen.setPenColor("000000");
+      Brain.Screen.setCursor(consoleLine + 2, 2);
+      Brain.Screen.print(text);
+      consoleLine++;
+    }
 
+    static void print(double text) {
+      if (consoleLine == 10) { //Over
+        consoleLine = 0;
+        init();
+      }
+      Brain.Screen.setFillColor("#ffffff");
+      Brain.Screen.setPenColor("000000");
+      Brain.Screen.setCursor(consoleLine + 2, 2);
+      sstream ss;
+      ss << text;
+      Brain.Screen.print(ss.str().c_str());
+      consoleLine++;
+    }
+
+    static void clear() {
+      consoleLine = 0;
+      Brain.Screen.setFillColor("#ffffff");
+      Brain.Screen.setPenColor("000000");
+      Brain.Screen.setCursor(consoleLine + 2, 2);
+    }
+  };
+  void move( float distance, float multiplier ) {
+    leftMotor.resetRotation();
+    rightMotor.resetRotation();
+
+    float margin = 1;
+    double error = distance / wheelCircumference * 360 - leftMotor.rotation(deg);
+    double previousError = 0;
+    const double maxAcceleration = 0.03;
+    while (!(leftMotor.rotation(deg) < distance / wheelCircumference * 360 + margin && leftMotor.rotation(deg) > distance / wheelCircumference * 360 - margin)) {
+      // Gets the previous error
+      error = distance / wheelCircumference * 360 - leftMotor.rotation(deg);
+      double speed = error;
+
+      /*if (fabs(speed - previousError) > maxAcceleration) {
+        //If accelerating
+        if (speed - previousError > 0) speed = previousError - maxAcceleration;
+        else speed = previousError + maxAcceleration;
+      }*/
+
+      Console::print(speed);
+
+      // Caps the error.
+      if (speed > 60) speed = 60;
+      if (speed < -60) speed = -60;
+      if (speed < 5 && error > 0) speed = 5;
+      if (speed > -5 && speed < 0) speed = -5;
+
+      leftMotor.spin(directionType::fwd, error * multiplier, rpm);
+      rightMotor.spin(directionType::fwd, error * multiplier, rpm);
+      leftFMotor.spin(directionType::fwd, error * multiplier, rpm);
+      rightFMotor.spin(directionType::fwd, error * multiplier, rpm);
+
+      wait(20, msec);
+      // How far the motor still needs to go.
+      previousError = error;
+    }
+    leftMotor.stop(hold);
+    rightMotor.stop(hold);
+  }
+  void moveTime( float miliseconds, float multiplier ) {
+    leftMotor.spin(directionType::fwd, 127 * multiplier, rpm);
+    rightMotor.spin(directionType::fwd, 127 * multiplier, rpm);
+    leftFMotor.spin(directionType::fwd, 127 * multiplier, rpm);
+    rightFMotor.spin(directionType::fwd, 127 * multiplier, rpm);
+    
+    wait(miliseconds, msec);
+
+    leftMotor.stop(hold);
+    rightMotor.stop(hold);
+  }
+/*--------------------*/
+
+/*-- Gnrl Functions --*/
+  void changeSpeed() {
+    if (speed == 1) speed = 3;
+    else speed = 1;
+    Controller.Screen.clearScreen();
+    Controller.Screen.setCursor(0, 0);
+    Controller.Screen.print("Speed:");
+    Controller.Screen.setCursor(2, 3);
+    Controller.Screen.print(1 / speed);
+  }
+/*--------------------*/
+
+/*---- Main Loops ----*/
+  //See details.txt for details
+  void autonomous(void) {    
+    moveTime(1000, 1);
+    firstLift.spin(fwd, 127, rpm);
+    secondLift.spin(directionType::rev, 127, rpm);
+    wait(5000, msec);
+    firstLift.stop(hold);
+    secondLift.stop(hold);
+    moveTime(1000, -1);   
+
+    complete = true;
+  }
   void usercontrol() {
-    GameObjects::leftMotor.stop(coast);
-    GameObjects::rightMotor.stop(coast);
+    leftMotor.stop(coast);
+    rightMotor.stop(coast);
 
-    Brain.Screen.setFillColor("#0000000");     
-    // Mid Label
-    if (mid) Brain.Screen.printAt(40, 50, "Mid (l=true, r=false) : true"); 
-    else Brain.Screen.printAt(40, 50, "Mid (l=true, r=false) : false");    
-    // Mid Buttons
-    UI::Button mid_true_button = UI::Button("#00ff00", Vector2f(40, 55), Vector2f::square(50), []{ mid = true; });
-    UI::Button mid_false_button = UI::Button("#ff0000", Vector2f(90, 55), Vector2f::square(50), []{ mid = false; });
-    
-    // Side Label
-    if (side == 1) Brain.Screen.printAt(40, 115, "Side : left");
-    else Brain.Screen.printAt(40, 115, "Side : right");
-    // Side Buttons
-    UI::Button side_left_button = UI::Button("#000000", Vector2f(40, 120), Vector2f::square(50), []{ side = 1; });
-    UI::Button side_right_button = UI::Button("#ffffff", Vector2f(90, 120), Vector2f::square(50), []{ side = -1; });
-    
-    resetControllerScreen();
+    Controller.ButtonA.pressed(changeSpeed);
 
     while (true) {
-      Vector2f mousePos = Vector2f(Brain.Screen.xPosition(), Brain.Screen.yPosition());
-
-      // If the autonmous is run, don't check button presses and clear screen.
-      if (autonCompletitonSteps == 0) {
-        // Runs functions of buttons through callbacks and clears the console if
-        // one of them is true so that the labels refresh.
-        if (UI::pollButtons(&mousePos)) {
-          Console::reset();
-
-          Brain.Screen.setFillColor("#0000000");
-          // Mid Label
-          if (mid) Brain.Screen.printAt(40, 50, "Mid (l=true, r=false) : true"); 
-          else Brain.Screen.printAt(40, 50, "Mid (l=true, r=false) : false");
-          // Mid Buttons
-          mid_true_button.render();
-
-          // Side Label
-          if (side == 1) Brain.Screen.printAt(40, 115, "Side : left"); 
-          else Brain.Screen.printAt(40, 115, "Side : right");
-
-          // Side Buttons
-          side_left_button.render();
-          side_right_button.render();
-        }
-      } else if (autonCompletitonSteps == 1) {
-        Console::reset();
-        autonCompletitonSteps++;
-      }
-
       // The difference between how fast the motors should  be going and the input.
-      float diffL = (GameObjects::leftMotor.velocity(rpm) - GameObjects::Controller.Axis3.value()) * .12; // /.12 = / 60 / 360
-      float diffR = (GameObjects::rightMotor.velocity(rpm) - GameObjects::Controller.Axis2.value()) * .12;
+      float diffL = (leftMotor.velocity(rpm) - Controller.Axis3.value()) * .12; // /.12 = / 60 / 360
+      float diffR = (rightMotor.velocity(rpm) - Controller.Axis2.value()) * .12;
       
-      if(!stick) {
-        if(GameObjects::Controller.Axis3.value() == 0) diffL = 0;
-        if(GameObjects::Controller.Axis2.value() == 0) diffR = 0;
-      }
-      
-      if (!move) { // Tank
-        GameObjects::leftMotor.spin(directionType::fwd, (GameObjects::Controller.Axis3.value() - diffL) * 2, velocityUnits::rpm);
-        GameObjects::rightMotor.spin(directionType::fwd, (GameObjects::Controller.Axis2.value() - diffR) * 2, velocityUnits::rpm);
-      } else { // Arcade
-        GameObjects::leftMotor.spin(directionType::fwd, GameObjects::Controller.Axis3.value() + GameObjects::Controller.Axis4.value(), velocityUnits::rpm);
-        GameObjects::rightMotor.spin(directionType::fwd, GameObjects::Controller.Axis3.value() - GameObjects::Controller.Axis4.value(), velocityUnits::rpm);
-      }
+      if(Controller.Axis3.value() == 0) diffL = 0;
+      if(Controller.Axis2.value() == 0) diffR = 0;
+        
+      leftMotor.spin(directionType::fwd, (Controller.Axis3.value() - diffL) * 2 / speed, velocityUnits::rpm);
+      rightMotor.spin(directionType::fwd, (Controller.Axis2.value() - diffR) * 2 / speed, velocityUnits::rpm);
+      leftFMotor.spin(directionType::fwd, (Controller.Axis3.value() - diffL) * 2 / speed, velocityUnits::rpm);
+      rightFMotor.spin(directionType::fwd, (Controller.Axis2.value() - diffR) * 2 / speed, velocityUnits::rpm);
 
-      float intake = 999 * (GameObjects::Controller.ButtonL1.pressing() - GameObjects::Controller.ButtonL2.pressing());
-      GameObjects::intakeLeft.spin(directionType::fwd, intake, velocityUnits::rpm);
-      GameObjects::intakeRight.spin(directionType::fwd, intake, velocityUnits::rpm);
-      GameObjects::lift.spin(directionType::fwd, intake, velocityUnits::rpm);
+      float intakeTop = 999 * (Controller.ButtonL1.pressing() - Controller.ButtonL2.pressing());
+      float intakeBtm = 999 * (Controller.ButtonR2.pressing() - Controller.ButtonR1.pressing());
+      intakeLeft.spin(directionType::fwd, intakeBtm, velocityUnits::rpm);
+      intakeRight.spin(directionType::fwd, intakeBtm, velocityUnits::rpm);
+      firstLift.spin(directionType::fwd, intakeBtm, velocityUnits::rpm);
+      secondLift.spin(directionType::fwd, intakeTop, velocityUnits::rpm);
       // Holds the lift
-      if (intake == 0) {
-        GameObjects::lift.stop(hold);
-      }
-
-      GameObjects::Controller.ButtonX.pressed([]{ // Change stick
-        stick = !stick; 
-        resetControllerScreen();
-      });
-      GameObjects::Controller.ButtonY.pressed([]{ // Change move
-        move = !move;
-        resetControllerScreen();
-      });
+      if (intakeBtm == 0) firstLift.stop(hold);
+      if(intakeTop == 0) secondLift.stop(hold);
 
       wait(20, msec);
     }    
   }
-}
+/*--------------------*/
 
 int main() {
   vexcodeInit();
-    
-  Syntech::Console::reset(); // Inits console.
+  Console::init();
 
-  Syntech::GameObjects::Competition.autonomous(Syntech::autonomous);
-  Syntech::GameObjects::Competition.drivercontrol(Syntech::usercontrol);
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
 
   while (true) {
     wait(100, msec);
